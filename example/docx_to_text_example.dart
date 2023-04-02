7f74de6f45e31c72eb1aeb62c1a59ab2621bb3f3
StarProxima <maks.prozorov1@yandex.ru>
2023-04-02 19:26:02 +0300

    initial commit

---
+++ example/docx_to_text_example.dart
@@ -0,0 +1,6 @@
+import 'package:docx_to_text/docx_to_text.dart';
+
+void main() {
+  var awesome = Awesome();
+  print('awesome: ${awesome.isAwesome}');
+}
---
+++ lib/docx_to_text.dart
@@ -0,0 +1,8 @@
+/// Support for doing something awesome.
+///
+/// More dartdocs go here.
+library docx_to_text;
+
+export 'src/docx_to_text_base.dart';
+
+// TODO: Export any libraries intended for clients of this package.
