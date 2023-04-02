b52a3634faa41fc7b56fa24273a8141855b4d72a
StarProxima <maks.prozorov1@yandex.ru>
2023-04-02 21:54:04 +0300

    Add example

--- example/docx_to_text_example.dart
+++ example/docx_to_text_example.dart
@@ -1,6 +1,13 @@
+import 'dart:io';
+
 import 'package:docx_to_text/docx_to_text.dart';
 
-void main() {
-  var awesome = Awesome();
-  print('awesome: ${awesome.isAwesome}');
+void main() async {
+  final file = File('example/test_sample.docx');
+
+  final bytes = await file.readAsBytes();
+
+  final text = docxToText(bytes, handleNumbering: true);
+
+  print(text);
 }
